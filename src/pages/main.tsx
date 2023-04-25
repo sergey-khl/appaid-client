529798b7953a3d6bf633af1ebfb86b21f43d1c3e
Sergey <sergeykhlynovskiy@gmail.com>
2023-04-24 19:35:01 -0600

    basic setup with server

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -10,58 +10,32 @@ export default function Main() {
   const SafeAreaView = Components['SafeAreaView'];
   const [component, setComponent] = useState(createElement(View, null));
 
-  const getComponent = () => {
-    const newComponent = `{
-      "jsx": {
-      "type": "TouchableOpacity",
-      "props": {
-      "activeOpacity": 0.8,
-      "onPress": "handleFacebookLogin",
-      "style": {
-      "backgroundColor": "#3b5998",
-      "borderRadius": 10,
-      "paddingVertical": 10,
-      "paddingHorizontal": 20,
-      "flexDirection": "row",
-      "alignItems": "center",
-      "justifyContent": "center",
-      "shadowColor": "#000",
-      "shadowOffset": {
-      "width": 0,
-      "height": 1
-      },
-      "shadowOpacity": 0.3,
-      "shadowRadius": 3,
-      "elevation": 5
-      }
-      },
-      "children": [
-      {
-      "type": "Text",
-      "props": {
-      "style": {
-      "color": "#fff",
-      "fontSize": 18,
-      "fontWeight": "bold",
-      "marginRight": 10
-      }
-      },
-      "children": "Connect with Facebook"
-      }
-      ]
-      }
-      }`;
-    
-    updateEmulator(newComponent);
-    return newComponent
+  // call server to get gpt output, if any
+  // TODO: add type to json component
+  const getComponent = (): any => {
+    return fetch('http://127.0.0.1:5000/components', {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(json => {
+        //console.log(json)
+        updateEmulator(json);
+      })
+      .catch(error => {
+        console.error(error);
+    });
   }
-  const updateEmulator = (component) => {
-    const comp = JSON.parse(component); 
-    const reactComponent = createElement(View, {style:{justifyContent:"center", alignItems: 'center', height: '100%', width: '100%'}, key: uuidv4()}, parseComponent(comp.jsx));
+
+  // use gpt output to create and render component
+  const updateEmulator = (component: any): void => {
+    console.log(component)
+    const reactComponent = createElement(View, {style:{justifyContent:"center", alignSelf: 'center', height: '100%', width: '100%'}, key: uuidv4()}, parseComponent(component.jsx));
     setComponent(reactComponent);
   }
 
-  const parseComponent = (jsonComponent) => {
+  // DFS alphabetically on json to get the react component
+  // TODO: ignore onPress, onHover, etc ...
+  const parseComponent = (jsonComponent: any): React.ReactElement => {
     
     if (jsonComponent.children.length == 0) {
       return createElement(Components[jsonComponent.type], {...jsonComponent.props, key: uuidv4()});
@@ -69,15 +43,14 @@ export default function Main() {
       return createElement(Components[jsonComponent.type], {...jsonComponent.props, key: uuidv4()}, jsonComponent.children);;
     }
 
-    let childs:React.ReactElement[] = [];
+    let children:React.ReactElement[] = [];
 
     jsonComponent.children.sort().forEach(child => {
-      childs.push(parseComponent(child));
+      children.push(parseComponent(child));
     });
-    return createElement(Components[jsonComponent.type], jsonComponent.props, childs);
+    return createElement(Components[jsonComponent.type], jsonComponent.props, children);
   }
 
- 
 
   return (
     <SafeAreaView style={styles.container}>
